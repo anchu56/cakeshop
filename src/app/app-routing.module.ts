0e940aa2c72f21db29ec4db1ebcfe791a70a2f81
=Ansaf Muhammed <anchu56@gmail.com>
2023-12-19 23:23:48 +0530

    First draft

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { LandingComponent } from './landing/landing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  {path:'landing',component:LandingComponent},
+  { path: '', redirectTo: '/landing', pathMatch: 'full' },
+  {path:'**',component:PagenotfoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { LandingComponent } from './landing/landing.component';
+import { LoginComponent } from './login/login.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { ProductlistComponent } from './productlist/productlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PagenotfoundComponent,
+    LandingComponent,
+    LoginComponent,
+    ContactusComponent,
+    TestimonialsComponent,
+    ProductlistComponent
   ],
   imports: [
     BrowserModule,
